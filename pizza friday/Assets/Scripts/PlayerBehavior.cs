b54b514a739e0fcc8e2f01fb419ea4be8b2faaf7
TomatoTheVeg <89602811+TomatoTheVeg@users.noreply.github.com>
2022-05-20 00:25:54 +0200

    Sticky walls and bouncy platforms
    added sticky walls and bouncy platforms mit folgenden Prefaben

--- pizza friday/Assets/Scripts/PlayerBehavior.cs
+++ pizza friday/Assets/Scripts/PlayerBehavior.cs
@@ -8,15 +8,21 @@ public class PlayerBehavior : MonoBehaviour
     [SerializeField] GameObject startPosition;
     Rigidbody2D rb;
     private Collider2D floor;
+    private Vector2 speed;
+    private float gravitySc;
 
     private void Start()
     {
         floor = deadFloor.GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        gravitySc = rb.gravityScale;
     }
     void Update()
     {
-        
+        if(rb.velocity!= Vector2.zero)
+        {
+            speed = rb.velocity;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,6 +37,40 @@ public class PlayerBehavior : MonoBehaviour
             //rb.velocity
             //collision.gameObject.transform.rotation.eulerAngles.z;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BouncePlatform p;
+        if (collision.gameObject.TryGetComponent<BouncePlatform>(out p))
+        {
+            rb.velocity = speed + 2*VectorProjection(speed, collision.collider.gameObject.transform.rotation);
+            Debug.Log("Bounce "+ 2 * VectorProjection(speed, collision.collider.gameObject.transform.rotation));
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        StickyWall w;
+        if (collision.gameObject.TryGetComponent<StickyWall>(out w))
+        {
+            rb.gravityScale = rb.gravityScale * (1 - w.stickiness);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        StickyWall w;
+        if (collision.gameObject.TryGetComponent<StickyWall>(out w))
+        {
+            rb.gravityScale = gravitySc;
+        }
+    }
+
+    private Vector2 VectorProjection(Vector2 speed, Quaternion transform)
+    {
+        Vector2 norm =new Vector2(Mathf.Sin(transform.eulerAngles.z), Mathf.Cos(transform.eulerAngles.z));
+        return norm*Mathf.Abs((norm*speed).x+(norm*speed).y);
     }
 
     public void TeleportToStart()
---
+++ pizza friday/Assets/Scripts/StickyWall.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWall : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float stickiness;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
