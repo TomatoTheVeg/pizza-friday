48aa202122884da8c1af841cab2751cb215ef29d
Igor009 <igormilev986@gmail.com>
2023-11-24 15:42:36 +0200

    VISUAL

---
+++ pizza friday/Assets/FigmaImporter/Editor/FigmaImporter.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FigmaImporter.Editor.EditorTree;
+using FigmaImporter.Editor.EditorTree.TreeData;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
+
+namespace FigmaImporter.Editor
+{
+    public class FigmaImporter : EditorWindow
+    {
+        [MenuItem("Window/FigmaImporter")]
+        static void Init()
+        {
+            FigmaImporter window = (FigmaImporter) EditorWindow.GetWindow(typeof(FigmaImporter));
+            window.Show();
+        }
+
+        private static FigmaImporterSettings _settings = null;
+        private static GameObject _rootObject;
+        private static List<Node> _nodes = null;
+        private MultiColumnLayout _treeView;
+        private string _lastClickedNode = String.Empty;
+        
+        private static string _fileName;
+        private static string _nodeId;
+        private float _scale = 1f;
+
+        Dictionary<string, Texture2D> _texturesCache = new Dictionary<string, Texture2D>();
+
+        public float Scale => _scale;
+
+        void OnGUI()
+        {
+            if (_settings == null)
+                _settings = FigmaImporterSettings.GetInstance();
+
+            int currentPosY = 0;
+            if (GUILayout.Button("OpenOauthUrl"))
+            {
+                OpenOauthUrl();
+            }
+
+            _settings.ClientCode = EditorGUILayout.TextField("ClientCode", _settings.ClientCode);
+            _settings.State = EditorGUILayout.TextField("State", _settings.State);
+            
+            EditorUtility.SetDirty(_settings);
+
+            if (GUILayout.Button("GetToken"))
+            {
+                _settings.Token = GetOAuthToken();
+            }
+
+            GUILayout.TextArea("Token:" + _settings.Token);
+            _settings.Url = EditorGUILayout.TextField("Url", _settings.Url);
+            _settings.RendersPath = EditorGUILayout.TextField("RendersPath", _settings.RendersPath);
+
+            _rootObject =
+                (GameObject) EditorGUILayout.ObjectField("Root Object", _rootObject, typeof(GameObject), true);
+            
+            _scale = EditorGUILayout.Slider("Scale", _scale, 0.01f, 4f);
+            
+            var redStyle = new GUIStyle(EditorStyles.label);
+
+            redStyle.normal.textColor = UnityEngine.Color.yellow;
+            EditorGUILayout.LabelField(
+                "Preview on the right side loaded via Figma API. It doesn't represent the final result!!!!", redStyle);
+
+            if (GUILayout.Button("Get Node Data"))
+            {
+                string apiUrl = ConvertToApiUrl(_settings.Url);
+                GetNodes(apiUrl);
+            }
+
+            if (_nodes != null)
+            {
+                DrawAdditionalButtons();
+                DrawNodeTree();
+                DrawPreview();
+                ShowExecuteButton();
+            }
+        }
+
+        private void DrawAdditionalButtons()
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("To Generate"))
+                SwitchNodesToGenerate();
+            if (GUILayout.Button("To Transform"))
+                SwitchNodesToTransform();
+#if VECTOR_GRAHICS_IMPORTED
+            if (GUILayout.Button("To SVG"))
+                SwitchSVGToTransform();
+#endif
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void SwitchSVGToTransform()
+        {
+            var nodesTreeElements = _treeView.TreeView.treeModel.Data;
+            NodesAnalyzer.AnalyzeSVGMode(_nodes, nodesTreeElements);
+        }
+
+        private void SwitchNodesToTransform()
+        {
+            var nodesTreeElements = _treeView.TreeView.treeModel.Data;
+            NodesAnalyzer.AnalyzeTransformMode(_nodes, nodesTreeElements);   
+        }
+
+        private void SwitchNodesToGenerate()
+        {
+            var nodesTreeElements = _treeView.TreeView.treeModel.Data;
+            NodesAnalyzer.AnalyzeRenderMode(_nodes, nodesTreeElements);
+        }
+
+        private void DrawPreview()
+        {
+            var lastRect = GUILayoutUtility.GetLastRect();
+            var widthMax = position.width / 2f;
+            var heightMax = this.position.height - lastRect.yMax - 50;
+            var height = heightMax;
+            var width = widthMax;
+            _texturesCache.TryGetValue(_lastClickedNode, out var lastLoadedPreview);
+            if (lastLoadedPreview != null)
+            {
+                CalculatePreviewSize(lastLoadedPreview, widthMax, heightMax, out width, out height);
+            }
+
+            var previewRect = new Rect(position.width / 2f, lastRect.yMax + 20, width, height);
+            if (lastLoadedPreview != null)
+                GUI.DrawTexture(previewRect, lastLoadedPreview);
+        }
+
+        private void CalculatePreviewSize(Texture2D lastLoadedPreview, float widthMax, float heightMax, out float width,
+            out float height)
+        {
+            if (lastLoadedPreview.width < widthMax && lastLoadedPreview.height < heightMax)
+            {
+                width = lastLoadedPreview.width;
+                height = lastLoadedPreview.height;
+            }
+            else
+            {
+                width = widthMax;
+                height = widthMax * lastLoadedPreview.height / lastLoadedPreview.width;
+                if (height > heightMax)
+                {
+                    height = heightMax;
+                    width = heightMax * lastLoadedPreview.width / lastLoadedPreview.height;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_treeView != null && _treeView.TreeView != null)
+                _treeView.TreeView.OnItemClick -= ItemClicked;
+            _treeView = null;
+            _nodes = null;
+            foreach (var texture in _texturesCache)
+            {
+                DestroyImmediate(texture.Value);
+            }
+
+            _texturesCache.Clear();
+        }
+
+        private void DrawNodeTree()
+        {
+            bool justCreated = false;
+            if (_treeView == null)
+            {
+                _treeView = new MultiColumnLayout();
+                justCreated = true;
+            }
+
+            var lastRect = GUILayoutUtility.GetLastRect();
+            var width = position.width / 2f;
+            var treeRect = new Rect(0, lastRect.yMax + 20, width, this.position.height - lastRect.yMax - 50);
+            _treeView.OnGUI(treeRect, _nodes);
+            var nodesTreeElements = _treeView.TreeView.treeModel.Data;
+            if (justCreated)
+            {
+                _treeView.TreeView.OnItemClick += ItemClicked;
+                NodesAnalyzer.AnalyzeRenderMode(_nodes, nodesTreeElements);
+                LoadAllRenders(nodesTreeElements);
+            }
+
+            NodesAnalyzer.CheckActions(_nodes, nodesTreeElements);
+        }
+
+        private async void LoadAllRenders(IList<NodeTreeElement> nodesTreeElements)
+        {
+            if (nodesTreeElements == null || nodesTreeElements.Count == 0)
+                return;
+            await Task.WhenAll(nodesTreeElements.Select(x => GetImage(x.figmaId)));
+            _lastClickedNode = nodesTreeElements[0].figmaId;
+        }
+
+        private async void ItemClicked(string obj)
+        {
+            Debug.Log($"[FigmaImporter] {obj} clicked");
+            _lastClickedNode = obj;
+            if (!_texturesCache.TryGetValue(obj, out var tex))
+            {
+                await GetImage(obj, false);
+            }
+            Repaint();
+        }
+
+        private void ShowExecuteButton()
+        {
+            var lastRect = GUILayoutUtility.GetLastRect();
+            var buttonRect = new Rect(lastRect.xMin, this.position.height - 30, lastRect.width, 30f);
+            if (GUI.Button(buttonRect, "Generate nodes"))
+            {
+                string apiUrl = ConvertToApiUrl(_settings.Url);
+                GetFile(apiUrl);
+            }
+        }
+
+        public async Task GetNodes(string url)
+        {
+            OnDestroy();
+            _nodes = await GetNodeInfo(url);
+            FigmaNodesProgressInfo.HideProgress();
+        }
+
+        private string ConvertToApiUrl(string s)
+        {
+            var substrings = s.Split('/');
+            var length = substrings.Length;
+            bool isNodeUrl = substrings[length - 1].Contains("node-id");
+            _fileName = substrings[length - 2];
+            if (!isNodeUrl)
+            {
+                return $"https://api.figma.com/v1/files/{_fileName}";
+            }
+
+            _nodeId = substrings[length - 1].Substring(substrings[length - 1].IndexOf("node-id=") + "node-id=".Length);
+            return $"https://api.figma.com/v1/files/{_fileName}/nodes?ids={_nodeId.Replace("-",":")}";
+        }
+
+        private const string ApplicationKey = "msRpeIqxmc8a7a6U0Z4Jg6";
+        private const string RedirectURI = "https://manakhovn.github.io/figmaImporter";
+
+        private const string OAuthUrl =
+            "https://www.figma.com/oauth?client_id={0}&redirect_uri={1}&scope=file_read&state={2}&response_type=code";
+
+        public void OpenOauthUrl()
+        {
+            var state = Random.Range(0, Int32.MaxValue);
+            string formattedOauthUrl = String.Format(OAuthUrl, ApplicationKey, RedirectURI, state.ToString());
+            Application.OpenURL(formattedOauthUrl);
+        }
+
+        private const string ClientSecret = "VlyvMwuA4aVOm4dxcJgOvxbdWsmOJE";
+
+        private const string AuthUrl =
+            "https://www.figma.com/api/oauth/token?client_id={0}&client_secret={1}&redirect_uri={2}&code={3}&grant_type=authorization_code";
+
+        private string GetOAuthToken()
+        {
+            WWWForm form = new WWWForm();
+            string request = String.Format(AuthUrl, ApplicationKey, ClientSecret, RedirectURI, _settings.ClientCode);
+            using (UnityWebRequest www = UnityWebRequest.Post(request, form))
+            {
+                www.SendWebRequest();
+
+                while (!www.isDone)
+                {
+                }
+
+                if (www.isNetworkError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    var result = www.downloadHandler.text;
+                    Debug.Log(result);
+                    return JsonUtility.FromJson<AuthResult>(result).access_token;
+                }
+            }
+
+            return "";
+        }
+
+        private async void GetFile(string fileUrl)
+        {
+            if (_rootObject == null)
+            {
+                Debug.LogError($"[FigmaImporter] Root object is null. Please add reference to a Canvas or previous version of the object");
+                return;
+            }
+
+            if (_nodes == null)
+            {
+                FigmaNodesProgressInfo.CurrentNode = FigmaNodesProgressInfo.NodesCount = 0;
+                FigmaNodesProgressInfo.CurrentTitle = "Loading nodes info";
+                await GetNodes(fileUrl);
+            }
+
+            FigmaNodeGenerator generator = new FigmaNodeGenerator(this);
+            foreach (var node in _nodes)
+            {
+                var nodeTreeElements = _treeView.TreeView.treeModel.Data;
+                await generator.GenerateNode(node, _rootObject, nodeTreeElements);
+            }
+
+            FigmaNodesProgressInfo.HideProgress();
+        }
+
+        private async Task<List<Node>> GetNodeInfo(string nodeUrl)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(nodeUrl))
+            {
+                www.SetRequestHeader("Authorization", $"Bearer {_settings.Token}");
+                www.SendWebRequest();
+                while (!www.isDone && !www.isNetworkError)
+                {
+                    FigmaNodesProgressInfo.CurrentInfo = "Loading nodes info";
+                    FigmaNodesProgressInfo.ShowProgress(www.downloadProgress);
+                    await Task.Delay(100);
+                }
+
+                FigmaNodesProgressInfo.HideProgress();
+
+                if (www.isNetworkError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    var result = www.downloadHandler.text;
+                    FigmaParser parser = new FigmaParser();
+                    return parser.ParseResult(result);
+                }
+
+                FigmaNodesProgressInfo.HideProgress();
+            }
+
+            return null;
+        }
+
+        private const string ImagesUrl = "https://api.figma.com/v1/images/{0}?ids={1}&svg_include_id=true&format=png&scale={2}";
+
+        public async Task<Texture2D> GetImage(string nodeId, bool showProgress = true)
+        {
+            if (_texturesCache.TryGetValue(nodeId, out var tex))
+            {
+                return _texturesCache[nodeId];
+            }
+
+            WWWForm form = new WWWForm();
+            string request = string.Format(ImagesUrl, _fileName, nodeId, _scale);
+            var requestResult = await MakeRequest<string>(request, showProgress);
+            var substrs = requestResult.Split('"');
+            FigmaNodesProgressInfo.CurrentInfo = "Loading node texture";
+            foreach (var s in substrs)
+            {
+                if (s.Contains("http"))
+                {
+                    var texture = await LoadTextureByUrl(s, showProgress);
+                    _texturesCache[nodeId] = texture;
+                    return texture;
+                }
+            }
+
+            return null;
+        }
+
+#if VECTOR_GRAHICS_IMPORTED
+        private const string SvgImagesUrl = "https://api.figma.com/v1/images/{0}?ids={1}&format=svg";
+        public async Task<byte[]> GetSvgImage(string nodeId, bool showProgress = true)
+        {
+
+            WWWForm form = new WWWForm();
+            string request = string.Format(SvgImagesUrl, _fileName, nodeId);
+            var svgInfoRequest = await MakeRequest<string>(request, showProgress);
+            var substrs = svgInfoRequest.Split('"');
+            foreach (var str in substrs)
+                if (str.Contains("https"))
+                {
+                    var svgData = await MakeRequest<byte[]>(str, showProgress, false);
+                    return svgData;
+                }
+
+            return null;
+        }
+#endif
+        
+        private async Task<T> MakeRequest<T>(string request, bool showProgress, bool appendBearerToken = true) where T : class
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(request))
+            {
+                if (appendBearerToken)
+                {
+                    www.SetRequestHeader("Authorization", $"Bearer {_settings.Token}");
+                }
+
+                www.SendWebRequest();
+                while (!www.isDone && !www.isNetworkError)
+                {
+                    FigmaNodesProgressInfo.CurrentInfo = "Getting node image info";
+                    if (showProgress)
+                        FigmaNodesProgressInfo.ShowProgress(www.downloadProgress);
+                    await Task.Delay(100);
+                }
+
+                FigmaNodesProgressInfo.HideProgress();
+
+                if (www.isNetworkError)
+                {
+                    Debug.Log(www.error);
+                    return null;
+                }
+                else
+                {
+                    if (typeof(T) == typeof(string))
+                        return www.downloadHandler.text as T;
+                    return www.downloadHandler.data as T;
+                }
+            }
+        }
+
+        public string GetRendersFolderPath()
+        {
+            return _settings.RendersPath;
+        }
+
+        private async Task<Texture2D> LoadTextureByUrl(string url, bool showProgress = true)
+        {
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
+            {
+                request.SendWebRequest();
+                while (request.downloadProgress < 1f)
+                {
+                    if (showProgress)
+                        FigmaNodesProgressInfo.ShowProgress(request.downloadProgress);
+                    await Task.Delay(100);
+                }
+
+                if (request.isNetworkError || request.isHttpError)
+                    return null;
+                var data = request.downloadHandler.data;
+                Texture2D t = new Texture2D(0, 0);
+                t.LoadImage(data);
+                FigmaNodesProgressInfo.HideProgress();
+                return t;
+            }
+        }
+
+
+        [Serializable]
+        public class AuthResult
+        {
+            [SerializeField] public string access_token;
+            [SerializeField] public string expires_in;
+            [SerializeField] public string refresh_token;
+        }
+    }
+}
---
+++ pizza friday/Assets/FigmaImporter/Editor/FigmaNodeGenerator.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FigmaImporter.Editor.EditorTree.TreeData;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FigmaImporter.Editor
+{
+    public class FigmaNodeGenerator
+    {
+        Vector2 offset = Vector2.zero;
+        private RectTransform root = null;
+        private readonly FigmaImporter _importer;
+
+        public FigmaNodeGenerator(FigmaImporter importer)
+        {
+            _importer = importer;
+        }
+
+        public async Task GenerateNode(Node node, GameObject parent, IList<NodeTreeElement> nodeTreeElements)
+        {
+            FigmaNodesProgressInfo.CurrentNode ++;
+            FigmaNodesProgressInfo.CurrentInfo = "Node generation in progress";
+            FigmaNodesProgressInfo.ShowProgress(0f);
+            
+            //RendersFolderの有無の確認
+            GenerateRenderSaveFolder(_importer.GetRendersFolderPath());
+            
+            var boundingBox = node.absoluteBoundingBox;
+            if (parent == null)
+            {
+                throw new Exception("[FigmaImporter] Parent is null. Set the canvas reference.");
+            }
+
+            var isParentCanvas = parent.GetComponent<Canvas>();
+            
+            if (isParentCanvas)    
+                offset = boundingBox.GetPosition();
+            
+            GameObject nodeGo = null;
+            var treeElement = nodeTreeElements.First(x => x.figmaId == node.id);
+
+            if (treeElement.actionType != ActionType.None)
+            {
+                nodeGo = isParentCanvas? null: TransformUtils.TryToFindPreviouslyCreatedObject(parent, node.id);
+                RectTransform parentT = null;
+                RectTransform rectTransform = null;
+                if (nodeGo == null)
+                {
+                    nodeGo = new GameObject();
+                    parentT = parent.GetComponent<RectTransform>();
+                    if (isParentCanvas)
+                        root = parentT;
+                    nodeGo.name = $"{node.name} [{node.id}]";
+                    rectTransform = nodeGo.AddComponent<RectTransform>();
+                    TransformUtils.SetParent(parentT, rectTransform);
+                }
+                else
+                {
+                    rectTransform = (RectTransform) nodeGo.transform;
+                    parent = rectTransform.parent.gameObject;
+                    isParentCanvas = parent.GetComponent<Canvas>();
+                    if (isParentCanvas)    
+                        offset = boundingBox.GetPosition();
+                    parentT = (RectTransform)nodeGo.transform.parent;
+                }
+
+                TransformUtils.SetPosition(parentT, rectTransform, boundingBox, _importer, offset);
+                if (!isParentCanvas)
+                    TransformUtils.SetConstraints(parentT, rectTransform, node.constraints);
+                ImageUtils.SetMask(node, nodeGo);
+            }
+            
+            switch (treeElement.actionType)
+            {
+                case ActionType.None:
+                    break;
+                case ActionType.Render:
+                    if (treeElement.sprite != null)
+                    {
+                        ImageUtils.AddOverridenSprite(nodeGo, treeElement.sprite);
+                        break;
+                    }
+                    await ImageUtils.RenderNodeAndApply(node, nodeGo, _importer);
+                    break;
+#if VECTOR_GRAHICS_IMPORTED
+                case ActionType.SvgRender:
+                    if (treeElement.sprite != null)
+                    {
+                        ImageUtils.AddOverridenSvgSprite(nodeGo, treeElement.sprite);
+                        break;
+                    }
+                    await ImageUtils.RenderSvgNodeAndApply(node, nodeGo, _importer);
+                    break;
+#endif
+                case ActionType.Generate:
+                    if (treeElement.sprite != null)
+                    {
+                        ImageUtils.AddOverridenSprite(nodeGo, treeElement.sprite);
+                    }
+                    else
+                    {
+                        AddText(node, nodeGo);
+                        AddFills(node, nodeGo);
+                        if (node.children == null) return;
+                    }
+                    if (node.children == null) return;
+                    await Task.WhenAll(node.children.Select(x => GenerateNode(x, nodeGo, nodeTreeElements))); //todo: Need to fix the progress bar because of simultaneous nodes generation.
+                    break;
+                case ActionType.Transform:
+                    
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        
+
+        private void AddText(Node node, GameObject nodeGo)
+        {
+            if (node.type == "TEXT")
+            {
+                var tmp = TMPUtils.GetOrAddTMPComponentToObject(nodeGo);
+                tmp.text = node.characters;
+                var style = node.style;
+                TMPUtils.ApplyFigmaStyleToTMP(tmp, style, _importer.Scale);                
+                tmp.alignment = TMPUtils.FigmaAlignmentToTMP(style.textAlignHorizontal, style.textAlignVertical);
+                tmp.fontStyle = TMPUtils.FigmaFontStyleToTMP(style.textDecoration, style.textCase);
+                //tmp.characterSpacing = style.letterSpacing; //It doesn't work like that, need to make some calculations.
+            }
+        }
+
+        
+        
+
+
+        private void AddFills(Node node, GameObject nodeGo)
+        {
+            var gg = GetGradientsGenerator();
+            Image image = nodeGo.GetComponent<Image>();
+            if (node.fills.Length > 0f && image == null && nodeGo.GetComponent<Graphic>()==null)
+                image = nodeGo.AddComponent<Image>();
+            
+            var tmp = nodeGo.GetComponent<TextMeshProUGUI>();
+            
+            for (var index = 0; index < node.fills.Length; index++)
+            {
+                var fill = node.fills[index];
+                if (index != 0)
+                {
+                    var go = TransformUtils.InstantiateChild(nodeGo, fill.type);
+                    image = go.AddComponent<Image>();
+                }
+
+                switch (fill.type)
+                {
+                    case "SOLID":
+                        if (tmp != null)
+                            tmp.color = fill.color.ToColor();
+                        else
+                            image.color = fill.color.ToColor();
+                        break;
+                    case "GRADIENT_LINEAR" when tmp != null:
+                        var gradient = fill.gradientStops;
+                        tmp.enableVertexGradient = true;
+                        var firstColor = gradient.Length <= 0 ? UnityEngine.Color.white : ColorUtils.ConvertToUnityColor(gradient[0].color);
+                        var secondColor = gradient.Length <= 1 ? firstColor : ColorUtils.ConvertToUnityColor(gradient[1].color);
+                        var thirdColor = gradient.Length <= 2 ? UnityEngine.Color.white : ColorUtils.ConvertToUnityColor(gradient[2].color);
+                        var fourthColor = gradient.Length <= 3 ? thirdColor : ColorUtils.ConvertToUnityColor(gradient[3].color); 
+                        tmp.colorGradient = new VertexGradient(firstColor, secondColor, thirdColor, fourthColor);
+                        break;
+                    default:
+                        var tex = gg.GetTexture(fill, node.absoluteBoundingBox.GetSize(), 256);
+                        string fileName = $"{node.name}_{index.ToString()}.png";
+                        ImageUtils.SaveTexture(tex, $"/{_importer.GetRendersFolderPath()}/{fileName}");
+                        var sprite = ImageUtils.ChangeTextureToSprite($"Assets/{_importer.GetRendersFolderPath()}/{fileName}");
+                        image.sprite = sprite;
+                        break;
+                }
+
+                if (image != null) 
+                    image.enabled = fill.visible != "false";
+            }
+        }
+
+        private static GradientsGenerator GetGradientsGenerator()
+        {
+            var foundAssets = AssetDatabase.FindAssets("t:GradientsGenerator");
+            GradientsGenerator gg = null;
+            if (foundAssets.Length == 0)
+            {
+                gg = ScriptableObject.CreateInstance<GradientsGenerator>();
+                if (AssetDatabase.IsValidFolder("Assets/FigmaImporter/") == false)
+                {
+                    AssetDatabase.CreateFolder("Assets", "FigmaImporter");
+                    if (AssetDatabase.IsValidFolder("Assets/FigmaImporter/Editor") == false)
+                    {
+                        AssetDatabase.CreateFolder("Assets/FigmaImporter", "Editor");
+                    }
+                }
+                AssetDatabase.CreateAsset(gg, "Assets/FigmaImporter/Editor/GradientsGenerator.asset");
+                AssetDatabase.SaveAssets();
+            }
+            else
+            {
+                var gradientGeneratorId = foundAssets[0];
+                gg = AssetDatabase.LoadAssetAtPath<GradientsGenerator>(
+                        AssetDatabase.GUIDToAssetPath(gradientGeneratorId));
+            }
+            return gg;
+        }
+
+        private static void GenerateRenderSaveFolder(string path)
+        {
+            var fullPath = Path.Combine(Application.dataPath, path);
+            if (Directory.Exists(fullPath))
+            {
+                return;
+            }
+            Directory.CreateDirectory(fullPath);
+        }
+    }
+}
